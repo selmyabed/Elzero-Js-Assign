6ff301637f45a09ab0d902009b9195f7a7d88b8d
selmyabed <selmy.abed17@gamail.com>
2022-04-07 21:29:19 +0200

    week 1 is done

---
+++ week 1/main.js
@@ -0,0 +1,21 @@
+// document.write('<p>Hello From Js File</p>');
+// document.querySelector('p').style.color = "blue";
+// document.querySelector('p').style.fontSize = "80px"
+
+// window.onload = function() {
+//     document.querySelector('h1').style.color = "red";
+// }
+
+// window.alert('hello from alert');
+
+// document.write('document.write')
+
+// window.console.log('window console log')
+
+console.log("printing from console");
+window.console.log('%cprinting from %cconsole' , "text-transform: uppercase ; font-size: 20px;" , "color:white; background-color: red; font-size: 50px" )
+window.console.error('Go Away Mother Fucker');
+window.console.table(['selmy' , 'samir', 'abed' , 'mohamed']);
+
+let myName = 'osOs';
+document.write('hello sir,' + myName);
